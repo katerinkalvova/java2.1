4b8007e29a7ae24c020e3f1094eeb376be131cc6
Katerina Lvova <katerinkalvova@seznam.cz>
2023-04-20 10:51:22 +0200

    -a

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int amount = 20;
+
+        int bonus = price / amount;
+
+        System.out.println("Бонус " + bonus);
+
+    }
+}
